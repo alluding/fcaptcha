30d87de9c5252ddf5d60a96f9c0ed05eeecd2367
dancers <149972831+alluding@users.noreply.github.com>
2024-05-15 19:11:56 -0400

    Update utils.py

--- fcaptcha/utils.py
+++ fcaptcha/utils.py
@@ -28,12 +28,8 @@ def check_exc(response: Response) -> typing.Dict[str, typing.Any]:
         "Invalid API Key": InvalidAPIKey("The API key provided is invalid. Please check your key."),
         "Task not found": TaskNotFound("The task ID was not found. Please try creating a new task.")
     }
-
-    error: str = response.json().get("error") if not isinstance(
-        response.json().get("error"),
-        bool
-    ) else response.json().get("message")
-
+    error: str = response.json().get("error") if not isinstance(response.json().get("error"), bool) else response.json().get("message")
+    
     if error and error in ERROR_MAP:
         raise ERROR_MAP[error]
 
